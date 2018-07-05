d87e5f377a77e198343e3472902263703f2d6795
Eric Winebrenner <erwinebrenner@gmail.com>
2018-07-05 16:11:33 -0400

    fixed relative paths

--- src/Preloader.js
+++ src/Preloader.js
@@ -6,13 +6,13 @@ Game.Preloader.prototype = {
     },
     preload: function () {
         game.plugins.add(PhaserNineSlice.Plugin);
-        game.load.image('boy',"../img/Character_YoungBoy_sad.png");
-        game.load.spritesheet('grandpa',"../img/Character_Grandfather_happy.png",750,1000);
-        game.load.image('bg',"../img/Background_Office.png");
-        game.load.image('arrow',"../img/Arrow.png");
-        game.load.image('man',"../img/Character_AdultMan_happy.png");
-        game.load.nineSlice('speechBubble',"../img/Popup.png",78,78,78,78);
-        game.load.nineSlice('speechBubbleDark',"../img/SpeechBubble_Dark.png",48,48,48,48);
+        game.load.image('boy',"img/Character_YoungBoy_sad.png");
+        game.load.spritesheet('grandpa',"img/Character_Grandfather_happy.png",750,1000);
+        game.load.image('bg',"img/Background_Office.png");
+        game.load.image('arrow',"img/Arrow.png");
+        game.load.image('man',"img/Character_AdultMan_happy.png");
+        game.load.nineSlice('speechBubble',"img/Popup.png",78,78,78,78);
+        game.load.nineSlice('speechBubbleDark',"img/SpeechBubble_Dark.png",48,48,48,48);
         game.load.script('BlurX', 'https://cdn.rawgit.com/photonstorm/phaser-ce/master/filters/BlurX.js');
         game.load.script('BlurY', 'https://cdn.rawgit.com/photonstorm/phaser-ce/master/filters/BlurY.js');
     },
